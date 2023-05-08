b8dbfebf63244c503c0376052fcbbec8e989dd2a
subarnapkrl <subarnapokharel252@gmail.com>
2023-05-09 05:34:00 +0545

    Render-display-onScreen-on-starting-Game

--- script.js
+++ script.js
@@ -20,3 +20,39 @@ const GameBoard=(()=>{
     }
 
 })();
+
+//FACTORY FOR PLAYERS
+const createPlayer=(name,gameMark)=>{
+    return{
+        name,
+        gameMark
+    }
+}
+
+//MODULE FOR CONTROLLING THE GAME
+const Game=(()=>{
+    let players=[];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const startGame=()=>{
+        //Since there can be more than two players , we initialize player in FACTORY
+        players=[
+            createPlayer(document.querySelector("#playerOne").value,"X"),
+            createPlayer(document.querySelector("#playerTwo").value,"O")]
+        
+        currentPlayerIndex=0;
+        gameOver=false;
+        GameBoard.renderOnScreen();
+    }
+    return{
+        startGame
+    }
+
+})();
+
+
+const startButton=document.querySelector("#startGame");
+startButton.addEventListener("click",()=>{
+    Game.startGame();
+})
